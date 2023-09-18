1bf9f6923a4220befb97b39be8d2bacec8b37a4d
Artemis5 <30831219+gr8enigma@users.noreply.github.com>
2023-09-18 23:11:19 +0200

    Fix interface of function and corresponding code
    The interface was edited to fit the problem statement
    of the algorithm. A pointer to the resulting array now is returned.

--- Arrays/two_sum.c
+++ Arrays/two_sum.c
@@ -1,10 +1,12 @@
 // https://leetcode.com/problems/two-sum/
 #include <stdio.h>
+#include <stdlib.h>
 
-void twoSum(int* nums, int numsSize, int target, int* returnSize)  {
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int solution_Found = 0;
     int i = 0;
     int j;
+    int* result;
     while(i < (numsSize - 1) && !solution_Found)
     {
         j = i + 1;
@@ -14,22 +16,29 @@ void twoSum(int* nums, int numsSize, int target, int* returnSize)  {
         }
         if ((nums[i] + nums[j]) == target)
         {
-            printf("[%d, %d]\n", i, j);
+            *returnSize = 2;
+            result = (int *)malloc(*returnSize * sizeof(int));
+            if(result == NULL)
+                return result;
+            result[0] = i;
+            result[1] = j;
             solution_Found = 1;
+            return result;
         }    
         else
         {
             i++;
         }
     }
-    return;
+    return NULL;
 }
 
 int main(void)
 {
     int returnSize = 0;
     int array[20] = {0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-    twoSum(array, 20, 3, &returnSize);
+    int* result = twoSum(array, 20, 20, &returnSize);
+    printf("[%d, %d]\n", result[0], result[1]);
     
     return 0;
 }

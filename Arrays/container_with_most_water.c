cac934212069a34ad4c02e5818234452eb2c603a
Artemis5 <30831219+gr8enigma@users.noreply.github.com>
2023-09-19 21:09:56 +0200

    Optimize runtime to O(n)

--- Arrays/container_with_most_water.c
+++ Arrays/container_with_most_water.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
-#define MAX 9
+#define MAX 7
 
 int maxArea(int* height, int heightSize)    {
     int current_max_area = 0;
-    int current_min_height = 0;
     int current_area = 0;
-    int max_possible_area_in_run = 0;
-    for(int i = 0; i < heightSize - 1; i++) {
-        for(int j = i + 1; j < heightSize; j++) {
-            max_possible_area_in_run = height[i] * (heightSize - i);
-            if(max_possible_area_in_run < current_max_area)
-                break;
-            current_min_height = (height[j] < height[i]) ? height[j] : height[i];
-            current_area = current_min_height * (j - i);
-            if(current_area > current_max_area) {
-                current_max_area = current_area;
-            }
-        }
+    int h_l = 0; 
+    int h_r = heightSize - 1;
+    while(h_l < h_r)
+    {
+        current_area = (height[h_l] < height[h_r] ? (h_r - h_l) * height[h_l] : (h_r - h_l) * height[h_r]);
+        if (current_area > current_max_area)
+            current_max_area = current_area;
+        if (height[h_l] < height[h_r])
+            ++h_l;
+        else
+            --h_r;
     }
     return current_max_area;
 }
 
 
 int main(void)  {
-    int height[MAX] = {1,8,6,2,5,4,8,3,7};
+    int height[MAX] = {2,3,4,5,18,17,6};
     int heightSize = MAX;
     int area = 0;
     area = maxArea(height, heightSize);
